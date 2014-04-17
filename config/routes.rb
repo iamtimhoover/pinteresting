f7988bf0dbbda3c22391b742e8115ca56e2df07e
Tim Hoover <timothyhoov@gmail.com>
2014-04-16 22:20:06 -0400

    Added shop page and boortstrap custimizations:

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get "contact" => "pages#contact" #creates contact path
 
+  get "shop" => "pages#shop" #creates shop path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
